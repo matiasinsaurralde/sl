672ecf6e73c89acca31759b300ffe13c467fc795
Matias Insaurralde <matias@insaurral.de>
2016-06-25 02:07:11 -0400

    Implementando interfaz de nodo

--- ast/ast.go
+++ ast/ast.go
@@ -1,6 +1,7 @@
 package Ast
 
 import(
+  "github.com/matiasinsaurralde/sl/token"
   "os"
 )
 
@@ -13,8 +14,59 @@ type File struct {
   Comments []Comment
 }
 
+type Node interface {
+  Pos() token.Pos
+  End() token.Pos
+}
+
+type RoutineLiteral struct {
+  // Type *RoutineType
+  // Body BlockStatement
+  Body *BlockStatement
+
+  StartPos token.Pos
+  EndPos token.Pos
+}
+
+func( r *RoutineLiteral ) Pos() token.Pos {
+  return r.StartPos
+}
+
+func( r *RoutineLiteral ) End() token.Pos {
+  return r.EndPos
+}
+
+type RoutineType struct {
+
+}
+
+type BlockStatement struct {
+  Start token.Pos
+  List []Statement
+  End token.Pos
+}
+
+type Statement interface {
+  Node
+  StatementNode()
+}
+
+type PrintStatement struct {
+  Print token.Pos
+}
+
 type Comment struct {
   Text string
+  StartPos token.Pos
+  EndPos token.Pos
+}
+
+func( c *Comment ) Pos() token.Pos {
+  return c.StartPos
+}
+
+func (c *Comment ) End() token.Pos {
+  return c.EndPos
 }
 
 type Scope struct {
