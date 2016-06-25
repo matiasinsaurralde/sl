32a680a6d03a27ee8bf762b0eae261c689f25f95
Matias Insaurralde <matias@insaurral.de>
2016-06-25 06:23:35 -0400

    Jugando con las estructuras de AST

--- ast/ast.go
+++ ast/ast.go
@@ -48,7 +48,25 @@ type BlockStatement struct {
 
 type Statement interface {
   Node
-  StatementNode()
+}
+
+type ExpressionStatement struct {
+
+}
+
+type CallExpression struct {
+  Function string
+  Args []Expression
+  StartPos token.Pos
+  EndPos token.Pos
+}
+
+func( c *CallExpression ) Pos() token.Pos {
+  return c.StartPos
+}
+
+func( c *CallExpression ) End() token.Pos {
+  return c.EndPos
 }
 
 type PrintStatement struct {
@@ -61,6 +79,61 @@ type Comment struct {
   EndPos token.Pos
 }
 
+type GenericDeclaration struct {
+  Name string
+  Values []Expression
+  StartPos token.Pos
+  EndPos token.Pos
+}
+
+func( r *GenericDeclaration ) Pos() token.Pos {
+  return r.StartPos
+}
+
+func( r *GenericDeclaration ) End() token.Pos {
+  return r.EndPos
+}
+
+
+type BasicLiteral struct {
+  Value string
+  Kind token.Token
+  StartPos token.Pos
+  EndPos token.Pos
+}
+
+func( b *BasicLiteral ) Pos() token.Pos {
+  return b.StartPos
+}
+
+func( b *BasicLiteral ) End() token.Pos {
+  return b.EndPos
+}
+
+type BinaryExpression struct {
+  X Expression
+  StartPos token.Pos
+  EndPos token.Pos
+  Y Expression
+}
+
+func( b *BinaryExpression ) Pos() token.Pos {
+  return b.StartPos
+}
+
+func( b *BinaryExpression ) End() token.Pos {
+  return b.EndPos
+}
+
+type Expression interface {
+  Node
+}
+
+type Variable struct {
+  Name string
+  Value interface{}
+}
+
 func( c *Comment ) Pos() token.Pos {
   return c.StartPos
 }
@@ -69,7 +142,22 @@ func (c *Comment ) End() token.Pos {
   return c.EndPos
 }
 
+type Declaration struct {
+  StartPos token.Pos
+  EndPos token.Pos
+}
+
+func( d *Declaration ) Pos() token.Pos {
+  return d.StartPos
+}
+
+func( d *Declaration ) End() token.Pos {
+  return d.EndPos
+}
+
 type Scope struct {
+  Declarations []Node
+  Nodes []Node
 }
 
 type FuncDeclaration struct {
