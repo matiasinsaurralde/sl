4a1503ac51b96cc75b789ec4373bd2c7460775c6
Matias Insaurralde <matias@insaurral.de>
2016-06-25 06:23:53 -0400

    Ajustando el parser de bloques

--- parser/parser.go
+++ parser/parser.go
@@ -12,14 +12,147 @@ import(
   "fmt"
 )
 
-func parseBlockStatement( st *Ast.BlockStatement, body string ) {
-  fmt.Println( "ParseBlockStatement:", st, "\n")
+func parseBlockStatement( blockStatement *Ast.BlockStatement, body string ) {
   reader := strings.NewReader(body)
   scanner := bufio.NewScanner( reader )
 
+  var expect token.Token
+  expect = -1
+
+  var node Ast.Node
+
   for scanner.Scan() {
-    fmt.Println("bufio.Scanner, statement:", scanner.Text() )
+    rawStatement := scanner.Text()
+    rawStatement = strings.Replace(rawStatement, "\n", "", -1)
+    if len(rawStatement) > 0 {
+      fmt.Println(" * Statement:", rawStatement )
+      buf := make([]byte, 0)
+
+      for _, ch := range rawStatement {
+
+        switch ch {
+        case 32: // " "
+        case 40: // (
+          tok := token.Lookup(string(buf))
+
+          switch tok {
+          case token.PRINT:
+            CallExpression := &Ast.CallExpression{
+              Function: string(buf),
+            }
+            node = CallExpression
+            expect = token.EXPR
+            buf = make([]byte, 0)
+          default:
+            buf = make([]byte, 0)
+          }
+        case 41: // ")"
+          if expect == token.EXPR {
+            rawExpressions := string(buf)
+
+            var CallExpression *Ast.CallExpression
+            CallExpression = node.(*Ast.CallExpression)
+
+            parseExpressions(&CallExpression.Args, rawExpressions)
+
+            var statement Ast.Statement
+            statement = CallExpression
+
+            blockStatement.List = append(blockStatement.List, statement)
+
+            expect = -1
+          }
+        default:
+          buf = append(buf, byte(ch))
+        }
+      }
+    }
   }
+}
+
+func parseExpression( expr string ) Ast.Expression {
+  // e := Ast.Expression{}
+  var e Ast.Expression
+  if strings.Index( expr, "+") > 0 {
+
+    splits := strings.Split(expr, "+")
+
+    b := &Ast.BinaryExpression{
+      X: &Ast.BasicLiteral{Value: splits[0]},
+      Y: &Ast.BasicLiteral{Value: splits[1]},
+    }
+    e = b
+
+    // b.X = literalX
+    // b.Y = literalY
+    return e
+  }
+
+  // Basic literal?
+  b := &Ast.BasicLiteral{
+    Value: expr,
+  }
+  return b
+}
+
+func parseExpressions( expressions *[]Ast.Expression, expr string ) {
+  e := parseExpression(expr)
+  *expressions = append( *expressions, e )
+}
+
+func parseDeclarations( body string ) []Ast.Node {
+
+  declarations := make([]Ast.Node, 0)
+
+  body = strings.Replace(body, "\n", "", -1)
+  body = strings.Replace(body, " ", "", -1)
+
+  if len(body) == 0 {
+    return declarations
+  }
+
+  var splits []string
+
+  splits = strings.Split(body, ":")
+
+  var node Ast.Node
+
+  // x : type (no value)
+  if len(splits) == 2 {
+    node = &Ast.GenericDeclaration{
+      Name: splits[0],
+    }
+    fmt.Println(" * Node:", node)
+
+    declarations = append(declarations, node)
+
+    return declarations
+  }
+
+  splits = strings.Split(body, "=")
+
+  // x = value (literal)
+  if len(splits) == 2 {
+    expressions := make([]Ast.Expression, 0)
+    genericDeclaration := &Ast.GenericDeclaration{
+      Name: splits[0],
+      Values: make([]Ast.Expression, 0),
+    }
+
+    node = genericDeclaration
+
+    fmt.Println(" * Node:", node)
+
+    parseExpressions( &expressions, splits[1])
+    // fmt.Println("Expressions:", splits[1], expressions[0])
+    // fmt.Println("* Node:", node)
+
+    declarations = append(declarations, node)
+
+    return declarations
+  }
+
+  return declarations
 }
 
 func ParseFile( filename string ) ( f *Ast.File, err error ) {
@@ -44,9 +177,12 @@ func ParseFile( filename string ) ( f *Ast.File, err error ) {
   var expect token.Token
   expect = -1
 
-  fmt.Println("Parser initialization")
+  fmt.Println("Parse...")
 
   var node Ast.Node
+
+  var globalScope Ast.Scope
+  globalScope = Ast.Scope{}
 
   for {
     _, err := r.Read(buf)
@@ -54,12 +190,9 @@ func ParseFile( filename string ) ( f *Ast.File, err error ) {
     var keep bool
     keep = false
 
-    // fmt.Println("Read ", n, "bytes, buf: ", buf, string(buf), string(v))
-
-    // fmt.Println("expect", expect, string(v))
-
     switch buf[0] {
     case 32: // " "
+
       tok := token.Lookup(string(v))
 
       if expect == -1 {
@@ -76,12 +209,20 @@ func ParseFile( filename string ) ( f *Ast.File, err error ) {
           keep = true
         case token.END:
           keep = true
+        case token.VAR:
+          // fmt.Println("?Var", string(v), len(v))
+          keep = true
         default:
           v = make([]byte, 0)
         }
       }
     case 10: // "\n"
-      tok := token.Lookup(string(v))
+
+      value := string(v)
+      value = strings.Replace(value, "\n", "", -1)
+
+      tok := token.Lookup(value)
+
       switch expect {
       case token.IDENT:
         fmt.Println("\n- Found a program: ", string(v))
@@ -90,6 +231,7 @@ func ParseFile( filename string ) ( f *Ast.File, err error ) {
       case token.COMMENT_END:
         keep = true
       case token.END:
+
         s := string(v)
 
         endSt := s[ len(s)-3 : len(s) ]
@@ -102,17 +244,32 @@ func ParseFile( filename string ) ( f *Ast.File, err error ) {
 
           body := s[0:len(s)-3]
 
-          parseBlockStatement(routineLiteral.Body, body )
+          parseBlockStatement( routineLiteral.Body, body )
 
           expect = -1
           v = make([]byte, 0)
+
+          // fmt.Println("\nBlock ends")
+
         } else {
           keep = true
         }
+      case token.VAR:
+        keep = true
+        // fmt.Println("Var declaration...", string(v))
+        declarations := parseDeclarations(string(v))
+
+        for _, d := range declarations {
+          globalScope.Declarations = append( globalScope.Declarations, d )
+        }
+
+        v = make([]byte, 0)
       }
 
       switch tok {
       case token.START:
+        fmt.Println("\n- Found a block\n")
+
         expect = token.END
         v = make([]byte,0)
 
@@ -124,6 +281,13 @@ func ParseFile( filename string ) ( f *Ast.File, err error ) {
         }
 
         node = routineLiteral
+
+        globalScope.Nodes = append(globalScope.Nodes, node)
+      case token.VAR:
+        fmt.Println("- Declaration:\n")
+        v = make([]byte, 0)
+        keep = true
+        expect = token.VAR
       }
     case 47: // "/"
       if expect == -1 {
@@ -160,6 +324,10 @@ func ParseFile( filename string ) ( f *Ast.File, err error ) {
       break
     }
   }
+
+  f.Scope  = &globalScope
+
+  fmt.Println( "\n- Scope:", f.Scope, "\n")
 
   return f, err
 }
