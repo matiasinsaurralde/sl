4d8b4213a5401e12af61381ecf43b135dfcb8be8
Matias Insaurralde <matias@insaurral.de>
2016-06-25 02:06:55 -0400

    Ajustando lectura de bloques

--- parser/parser.go
+++ parser/parser.go
@@ -7,8 +7,20 @@ import(
   "os"
   "bufio"
 
+  "strings"
+
   "fmt"
 )
+
+func parseBlockStatement( st *Ast.BlockStatement, body string ) {
+  fmt.Println( "ParseBlockStatement:", st, "\n")
+  reader := strings.NewReader(body)
+  scanner := bufio.NewScanner( reader )
+
+  for scanner.Scan() {
+    fmt.Println("bufio.Scanner, statement:", scanner.Text() )
+  }
+}
 
 func ParseFile( filename string ) ( f *Ast.File, err error ) {
 
@@ -24,13 +36,17 @@ func ParseFile( filename string ) ( f *Ast.File, err error ) {
 
   r := bufio.NewReader(file)
   buf := make([]byte, 1)
-
   v := make([]byte, 0)
+
+  startPos := 0
+  currentPos := 0
 
   var expect token.Token
   expect = -1
 
   fmt.Println("Parser initialization")
+
+  var node Ast.Node
 
   for {
     _, err := r.Read(buf)
@@ -39,6 +55,8 @@ func ParseFile( filename string ) ( f *Ast.File, err error ) {
     keep = false
 
     // fmt.Println("Read ", n, "bytes, buf: ", buf, string(buf), string(v))
+
+    // fmt.Println("expect", expect, string(v))
 
     switch buf[0] {
     case 32: // " "
@@ -56,11 +74,14 @@ func ParseFile( filename string ) ( f *Ast.File, err error ) {
         switch expect {
         case token.COMMENT_END:
           keep = true
+        case token.END:
+          keep = true
         default:
           v = make([]byte, 0)
         }
       }
     case 10: // "\n"
+      tok := token.Lookup(string(v))
       switch expect {
       case token.IDENT:
         fmt.Println("\n- Found a program: ", string(v))
@@ -68,16 +89,57 @@ func ParseFile( filename string ) ( f *Ast.File, err error ) {
         expect = -1
       case token.COMMENT_END:
         keep = true
+      case token.END:
+        s := string(v)
+
+        endSt := s[ len(s)-3 : len(s) ]
+        tok = token.Lookup(string(endSt))
+
+        if tok == token.END {
+
+          routineLiteral := node.(*Ast.RoutineLiteral)
+          routineLiteral.EndPos = token.Pos(currentPos)
+
+          body := s[0:len(s)-3]
+
+          parseBlockStatement(routineLiteral.Body, body )
+
+          expect = -1
+          v = make([]byte, 0)
+        } else {
+          keep = true
+        }
+      }
+
+      switch tok {
+      case token.START:
+        expect = token.END
+        v = make([]byte,0)
+
+        routineLiteral := &Ast.RoutineLiteral{
+          StartPos: token.Pos(currentPos),
+          Body: &Ast.BlockStatement{
+            List: make([]Ast.Statement, 0),
+          },
+        }
+
+        node = routineLiteral
       }
     case 47: // "/"
-      if expect == token.COMMENT_END {
-        // fmt.Println("Comment:", string(v))
-        fmt.Println( "\n- Found a comment...", string(v))
-        f.Comments = append( f.Comments, Ast.Comment{string(v)} )
-      }
       if expect == -1 {
         v = make([]byte, 0)
         expect = token.COMMENT_START
+
+        startPos = currentPos
+      }
+      if expect == token.COMMENT_END {
+        fmt.Println( "\n- Found a comment...", string(v))
+        comment := Ast.Comment{
+          StartPos: token.Pos(startPos), EndPos: token.Pos(currentPos), Text: string(v),
+        }
+        f.Comments = append( f.Comments, comment )
+        v = make([]byte, 0)
+        expect = -1
       }
     case 42: // "*"
       if expect == token.COMMENT_START {
@@ -91,27 +153,8 @@ func ParseFile( filename string ) ( f *Ast.File, err error ) {
     if keep {
       v = append(v, buf[0])
     }
-    /*
-    switch buf[0] {
-    case 32:
-      fmt.Println("v = ", v, string(v))
-      v = make([]byte, 0)
-    case 34:
-      fmt.Println("String starts or ends", string(v))
-      v = make([]byte, 0)
-    case 40:
-      fmt.Println("Initial parenthesis", string(v))
-      v = make([]byte, 0)
-    case 41:
-      fmt.Println("End parenthesis", string(v))
-      v = make([]byte,0)
-    case 10:
-      fmt.Println("CRLF", v)
-      v = make([]byte,0)
-    default:
-      v = append(v, buf[0])
-    }
-    */
+
+    currentPos++
 
     if err != nil {
       break
